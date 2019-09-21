5ea04304997dbcab862fbac73e689891965fe05b
carinama1 <jarpani31@gmail.com>
2019-09-21 16:58:05 +0700

    asd

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./pages/main";
 import Navbar from "./pages/navbar";
 import RestApi from "./pages/RESTapi";
-<<<<<<< HEAD
 import LoginComponent from "./pages/components/loginComponent";
 import "./css/style.css";
 
@@ -15,18 +14,6 @@ function App() {
         <LoginComponent />
         <Route path="/" exact component={Main} />
         <Route path="/Api" exact component={RestApi} />
-=======
-import "./css/style.css";
-function App() {
-  return (
-    <div className="App">
-      {/* <h1>{process.env.NODE_ENV}</h1> */}
-      <Router>
-        <Navbar />
-        <Route path="/" exact component={Main} />
-        <Route path="/Api" exact component={RestApi} />
-        {/* <Router path='login' exact component={} */}
->>>>>>> 5696d6003555631175b5a138468599ee30b0750e
       </Router>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-<<<<<<< HEAD
 import store from "./store";
 import { Provider } from "react-redux";
 
@@ -12,10 +11,6 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-=======
-
-ReactDOM.render(<App />, document.getElementById("root"));
->>>>>>> 5696d6003555631175b5a138468599ee30b0750e
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
